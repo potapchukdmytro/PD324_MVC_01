678cd20674e6204bbfe3d535857ed9ae784ad541
Dmytro Potapchuk <traiget@gmail.com>
2024-09-02 17:23:28 +0300

    update products seed data

--- PD324_01/Data/AppDbContext.cs
+++ PD324_01/Data/AppDbContext.cs
@@ -21,12 +21,12 @@ namespace PD324_01.Data
                 );
 
             modelBuilder.Entity<Product>().HasData(
-                new Product { Id = 1, CategoryId = 1, Price = 3700.0M, Description = "", Name = "AMD Ryzen 5 5500U", Image = "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRC98q4Vog6E0ThvsYJZ9M4zQYc7dC-BPrOut3rXMOpAHCgdRd4wlG7b7Y2Ml30vh9ijbrRhVrhizkWlyfQRB2AzspjWn3WTg4muTZFkWA&usqp=CAE" },
-                new Product { Id = 2, CategoryId = 1, Price = 26406.0M, Description = "", Name = "intel core i9 14900k", Image = "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTR0a-LCLAsE8kD7EyVZtQ4fWuOJFUr7om5GatUqr_LNAkbon0RnDxfWLE-uxZhFHuL_EWYtSRj3M5MTWlQsVymGKfBaDkbjBySMoIwj6qDdHa7xTmQOVD1&usqp=CAE" },
-                new Product { Id = 3, CategoryId = 1, Price = 512876.0M, Description = "", Name = "AMD Ryzen Threadripper PRO 7995WX", Image = "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTTuRvD4ecfGFUT5OPX7Y0DUMVCa91IHyzbeqwO3TY6UNwhpfJoB6Harwwm83LcDEfP1bYo34LvJH-PkaKODY8Xwh-x1c1R6pqW_oRaeWDdkMjMTSWIzN8-gg&usqp=CAE" },
-                new Product { Id = 4, CategoryId = 2, Price = 88999.0M, Description = "", Name = "Nvidia RTX 4090", Image = "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcT_OzFW3PueJdquyAqLvXML8ex14absxwEKb9_ai1zWa3_bzk-GccmBsWblN47cJQXndg_ni1lcz8v1y0xTs3BGiu8NSgRXCYRElUKdTvyKjySYjbPQIQz8&usqp=CAE" },
-                new Product { Id = 5, CategoryId = 2, Price = 7999.0M, Description = "", Name = "AMD RX 6600", Image = "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSN-HOzsGtF5sr9xFHO2ttuITyboTba__ey5QJXz5_vfQT2t_qdV9-qRnqgHlK4tvbY7DoprOEmbrqAW5VnDOZcdK2WkStUAMW-duANTnk&usqp=CAE" },
-                new Product { Id = 6, CategoryId = 2, Price = 6500.0M, Description = "", Name = "Nvidia GeForce GTX 1080", Image = "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSbElEyWjjPP1cYTHLoyhnkZ3EJDbFsXQD1pNvJe6bSr1F5uCmMFThovMt2hRaP8BASxdTA5P2M6WIGNKnNnIwHb6OKPIy0LoyD6GurgAXU4iEen_Foj5W4&usqp=CAE" }
+                new Product { Id = 1, CategoryId = 1, Price = 3700.0M, Description = "", Name = "AMD Ryzen 5 5500U", Image = "910632df-d958-45ee-a84e-1ec107893d66.webp" },
+                new Product { Id = 2, CategoryId = 1, Price = 26406.0M, Description = "", Name = "intel core i9 14900k", Image = "f1e28f88-7de9-4468-9673-4e78201a89fb.webp" },
+                new Product { Id = 3, CategoryId = 1, Price = 512876.0M, Description = "", Name = "AMD Ryzen Threadripper PRO 7995WX", Image = "8db30c71-35a1-4903-b279-dfc1c92bdd8d.webp" },
+                new Product { Id = 4, CategoryId = 2, Price = 88999.0M, Description = "", Name = "Nvidia RTX 4090", Image = "fd47a589-db2d-4804-b32b-40007955f610.webp" },
+                new Product { Id = 5, CategoryId = 2, Price = 7999.0M, Description = "", Name = "AMD RX 6600", Image = "8681144b-62b0-4344-a0d9-191d8cc8955b.webp" },
+                new Product { Id = 6, CategoryId = 2, Price = 6500.0M, Description = "", Name = "Nvidia GeForce GTX 1080", Image = "068c9a22-dc0b-4033-8ad4-c55ad2f1a444.webp" }
                 );
 
             modelBuilder.Entity<User>(b =>
---
+++ PD324_01/Migrations/20240902142308_Update images for products seed data.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PD324_01.Migrations
+{
+    /// <inheritdoc />
+    public partial class Updateimagesforproductsseeddata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Image",
+                value: "910632df-d958-45ee-a84e-1ec107893d66.webp");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Image",
+                value: "f1e28f88-7de9-4468-9673-4e78201a89fb.webp");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Image",
+                value: "8db30c71-35a1-4903-b279-dfc1c92bdd8d.webp");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Image",
+                value: "fd47a589-db2d-4804-b32b-40007955f610.webp");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Image",
+                value: "8681144b-62b0-4344-a0d9-191d8cc8955b.webp");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Image",
+                value: "068c9a22-dc0b-4033-8ad4-c55ad2f1a444.webp");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Image",
+                value: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRC98q4Vog6E0ThvsYJZ9M4zQYc7dC-BPrOut3rXMOpAHCgdRd4wlG7b7Y2Ml30vh9ijbrRhVrhizkWlyfQRB2AzspjWn3WTg4muTZFkWA&usqp=CAE");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Image",
+                value: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTR0a-LCLAsE8kD7EyVZtQ4fWuOJFUr7om5GatUqr_LNAkbon0RnDxfWLE-uxZhFHuL_EWYtSRj3M5MTWlQsVymGKfBaDkbjBySMoIwj6qDdHa7xTmQOVD1&usqp=CAE");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Image",
+                value: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTTuRvD4ecfGFUT5OPX7Y0DUMVCa91IHyzbeqwO3TY6UNwhpfJoB6Harwwm83LcDEfP1bYo34LvJH-PkaKODY8Xwh-x1c1R6pqW_oRaeWDdkMjMTSWIzN8-gg&usqp=CAE");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Image",
+                value: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcT_OzFW3PueJdquyAqLvXML8ex14absxwEKb9_ai1zWa3_bzk-GccmBsWblN47cJQXndg_ni1lcz8v1y0xTs3BGiu8NSgRXCYRElUKdTvyKjySYjbPQIQz8&usqp=CAE");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Image",
+                value: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSN-HOzsGtF5sr9xFHO2ttuITyboTba__ey5QJXz5_vfQT2t_qdV9-qRnqgHlK4tvbY7DoprOEmbrqAW5VnDOZcdK2WkStUAMW-duANTnk&usqp=CAE");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Image",
+                value: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSbElEyWjjPP1cYTHLoyhnkZ3EJDbFsXQD1pNvJe6bSr1F5uCmMFThovMt2hRaP8BASxdTA5P2M6WIGNKnNnIwHb6OKPIy0LoyD6GurgAXU4iEen_Foj5W4&usqp=CAE");
+        }
+    }
+}
